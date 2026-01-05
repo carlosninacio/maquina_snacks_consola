a91c60ebf52456e3c004ec3964552240224f01cc
carlosninacio <cigramirez@poligran.edu.co>
2026-01-05 15:22:57 -0500

    Clase entidad terminada
    java bean con atributos, constructores, metodos getters, setters, toString, hashcode y equals

--- src/main/java/entidad/Snack.java
+++ src/main/java/entidad/Snack.java
@@ -1,6 +1,7 @@
 package entidad;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Snack implements Serializable {
     private static int contadorSnacks = 0;
@@ -13,8 +14,53 @@ public class Snack implements Serializable {
     }
 
     public Snack(String nombre, double precio) {
-        this(); // Llamar al constructor vacio
+        this(); // Llamar al constructor vacio (Debe ser la primer linea)
         this.nombre = nombre;
         this.precio = precio;
+    }
+
+    public static int getContadorSnacks() {
+        return contadorSnacks;
+    }
+
+    public int getIdSnack() {
+        return idSnack;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSnack, nombre, precio);
     }
 }
